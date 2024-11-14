dd0d308cda29c744a5b27be6b2803aec1b90ba0e
Devin Sharp <sharpdevin467@gmail.com>
2024-11-13 18:51:49 -0800

    routes coded

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface JwtPayload {
+  username: string;
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  // TODO: verify the token exists and add the user data to the request object
+  const token = req.headers.authorization
+
+  if(!token) {
+    
+  }
+};
