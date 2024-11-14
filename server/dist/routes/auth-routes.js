a64c7590f09b7a739158ae078c1aeae802ddda10
Devin Sharp <sharpdevin467@gmail.com>
2024-11-14 00:12:41 -0800

    fixed fetch

--- server/dist/routes/auth-routes.js
+++ server/dist/routes/auth-routes.js
@@ -4,22 +4,20 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 export const login = async (req, res) => {
     // TODO: If the user exists and the password is correct, return a JWT token
-    const { username } = req.body.username;
-    const { password } = req.body.password;
+    const { username, password } = req.body;
     const user = await User.findOne({
         where: { username },
     });
     if (!user) {
-        return res.status(401).json({ message: 'No user found' });
+        res.status(401).json({ message: 'No user found' });
     }
-    const passwordIsValid = await bcrypt.compare(password, user.password);
+    const passwordIsValid = await bcrypt.compare(password, user.password || "");
     if (!passwordIsValid) {
-        return res.status(401).json({ message: 'No password found' });
+        res.status(401).json({ message: 'No password found' });
     }
     const secretKey = process.env.JWT_SECRET_KEY || '';
     const token = jwt.sign({ username }, secretKey, { expiresIn: '10h' });
-    console.log(token);
-    return res.json({ token });
+    res.json({ token });
 };
 const router = Router();
 // POST /login - Login a user
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,27 +5,25 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
-  const { username } = req.body.username;
-  const { password } = req.body.password;
+  const { username, password } = req.body;
 
   const user = await User.findOne({
     where: {username},
   });
 
   if(!user) {
-    return res.status(401).json({ message: 'No user found'});
+    res.status(401).json({ message: 'No user found'});
   }
 
-  const passwordIsValid = await bcrypt.compare(password, user.password);
+  const passwordIsValid = await bcrypt.compare(password, user!.password || "");
   if(!passwordIsValid) {
-    return res.status(401).json({ message: 'No password found'});
+    res.status(401).json({ message: 'No password found'});
   }
 
   const secretKey = process.env.JWT_SECRET_KEY || '';
 
   const token = jwt.sign({username}, secretKey, {expiresIn: '10h'});
-  console.log(token);
-  return res.json({token});
+  res.json({token});
 };
 
 const router = Router();
