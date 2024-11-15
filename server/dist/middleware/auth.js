3ff4da6b746563a53e85488323fb03b36a1032e6
Devin Sharp <sharpdevin467@gmail.com>
2024-11-14 20:58:07 -0800

    remove console logs

--- server/dist/middleware/auth.js
+++ server/dist/middleware/auth.js
@@ -2,13 +2,11 @@ import jwt from 'jsonwebtoken';
 export const authenticateToken = (req, res, next) => {
     // TODO: verify the token exists and add the user data to the request object
     const authCombine = req.headers.authorization;
-    console.log(authCombine);
     if (authCombine) {
         const token = authCombine.split(" ")[1];
         const sKey = process.env.JWT_SECRET_KEY || "";
         jwt.verify(token, sKey, (err, user) => {
             if (err) {
-                console.log(err);
                 return res.status(401).json({ message: "error" });
             }
             req.user = user;
@@ -16,7 +14,6 @@ export const authenticateToken = (req, res, next) => {
         });
     }
     else {
-        console.log("test");
         res.status(401);
     }
 };
